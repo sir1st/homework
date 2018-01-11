311acd3bd6faa82de4d6fc78d1837b281ed17082
sir1st <1174702930@qq.com>
2018-01-11 12:47:05 +0800

    final version 2.0

--- Project.py
+++ Project.py
@@ -4,9 +4,12 @@ from flask import Flask, request , send_from_directory , send_file,render_templa
 from flask_uploads import UploadSet, configure_uploads, IMAGES,\
  patch_request_class
 from werkzeug import secure_filename
-
+#coding:utf-8
+#from flask.ext.bootstrap import Bootstrap #专为Flask开发发拓展都暴露在flask.ext命名空间下，Flask-Bootstrap输出一个Bootstrap类
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap=Bootstrap(app)
 app.config['UPLOADED_PHOTOS_DEST'] = 'D:/upload'  # 文件储存地址
 
 photos = UploadSet('photos', IMAGES)
@@ -22,12 +25,12 @@ def upload_file():
         file_url = photos.url(filename)
         Transform.transform(app.config['UPLOADED_PHOTOS_DEST']+'/',filename)
         file_url2 = photos.url('temp.jpg')
-        return render_template('index.html') + '<br><img src=' + file_url + '>'+ '<br><img src=' + file_url2 + '>'
+        return render_template('test.html')+'<br><img src=' + file_url + '>'+ '<br><img src=' + file_url2 + '>'
     return render_template('index.html')
     
 @app.route("/download",methods=['GET','POST'])
 def download():
-    if request.method =='GET':
+    if request.method =='POST':
         print('download txt')
         directory = app.config['UPLOADED_PHOTOS_DEST']
         filename='temp.txt'
