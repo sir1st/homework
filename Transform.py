ca9c1737ffcff17e8fde884a60970d65aee3fe72
sir1st <1174702930@qq.com>
2018-01-11 20:16:35 +0800

    emmmm....whatever, I sware this is the last change

--- Transform.py
+++ Transform.py
@@ -2,16 +2,16 @@
 from PIL import Image
 import Tojpg
 
-codeLib = '''@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. '''#生成字符画所需的字符集
+codeLib = '''@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. '''
 count = len(codeLib)
 
 def transform1(image_file):
-    image_file = image_file.convert("L")#转换为黑白图片，参数"L"表示黑白模式
+    image_file = image_file.convert("L")
     codePic = ''
-    for h in range(0,image_file.size[1]):  #size属性表示图片的分辨率，'0'为横向大小，'1'为纵向
+    for h in range(0,image_file.size[1]):  
         for w in range(0,image_file.size[0]):
-            gray = image_file.getpixel((w,h)) #返回指定位置的像素，如果所打开的图像是多层次的图片，那这个方法就返回一个元组
-            codePic = codePic + codeLib[int(((count-1)*gray)/256)]#建立灰度与字符集的映射
+            gray = image_file.getpixel((w,h))
+            codePic = codePic + codeLib[int(((count-1)*gray)/256)]
         codePic = codePic+'\r\n'
     return codePic
 
@@ -26,11 +26,9 @@ def transform2(image_file):
     return codePic
 
 def transform(directory,filename):
-    #print('transform')
     fp = open(directory+filename,'rb')
     image_file = Image.open(fp)
-    image_file=image_file.resize((int(image_file.size[0]*1.1), int(image_file.size[1]*0.5)))#调整图片大小
-    #print (u'Info:',image_file.size[0],' ',image_file.size[1],' ',count)
+    image_file=image_file.resize((int(image_file.size[0]*1.1), int(image_file.size[1]*0.5)))
 
     tmp = open(directory+'temp.txt','w')
     tmp.write(transform1(image_file))
